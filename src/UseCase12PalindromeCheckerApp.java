aa51d54ec17d0731a839929a3f085f95ebe62f4c
cc1264kishore <cc1264@srmist.edu.in>
2026-03-11 10:23:00 +0530

    UC12: Strategy pattern for palindrome algorithms

---
+++ src/UseCase12PalindromeCheckerApp.java
@@ -0,0 +1,70 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean check(String word);
+}
+
+// Stack Strategy
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean check(String word) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        return word.equals(reversed);
+    }
+}
+
+// Deque Strategy
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String word) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Main Application
+public class UseCase12PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        // Choose strategy
+        PalindromeStrategy strategy = new StackStrategy();
+        //PalindromeStrategy strategy = new DequeStrategy();
+
+        if (strategy.check(word)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+    }
+}
