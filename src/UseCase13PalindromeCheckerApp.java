abda309c129d770f9ed1ec573de6aae67dc45e7c
cc1264kishore <cc1264@srmist.edu.in>
2026-03-11 10:24:52 +0530

    UC13: Performance comparison of palindrome algorithms

---
+++ src/UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,53 @@
+public class UseCase13PalindromeCheckerApp {
+
+    // Reverse string method
+    public static boolean reverseMethod(String word) {
+
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        return word.equals(reversed);
+    }
+
+    // Two pointer method
+    public static boolean twoPointerMethod(String word) {
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        // Reverse method timing
+        long startTime = System.nanoTime();
+        reverseMethod(word);
+        long endTime = System.nanoTime();
+
+        long reverseTime = endTime - startTime;
+
+        // Two pointer method timing
+        startTime = System.nanoTime();
+        twoPointerMethod(word);
+        endTime = System.nanoTime();
+
+        long pointerTime = endTime - startTime;
+
+        System.out.println("Reverse Method Time: " + reverseTime + " ns");
+        System.out.println("Two Pointer Method Time: " + pointerTime + " ns");
+    }
+}
